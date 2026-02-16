6a8e23e9ac89dbbf5029125c2b6ca7439a0230ad
katlalala <katrina.skudrite@sk.lvt.lv>
2026-02-16 16:07:24 +0200

    Labots

--- Assets/Scripts/CharacterControllerScript.cs
+++ Assets/Scripts/CharacterControllerScript.cs
@@ -8,36 +8,38 @@ public class CharacterControllerScript : MonoBehaviour
     private Animator animator;
     private SpriteRenderer spriteRenderer;
     private float moveInput;
+
+
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
         animator = GetComponentInChildren<Animator>();
-        spriteRenderer = GetComponent<SpriteRenderer>();
+        spriteRenderer = GetComponentInChildren<SpriteRenderer>();
     }
 
-    // Update is called once per frame
+
     void Update()
     {
         moveInput = 0;
 
-        if(Keyboard.current.leftArrowKey.isPressed )
+        if (Keyboard.current.leftArrowKey.isPressed)
         {
             moveInput = -1;
         }
-        else if(Keyboard.current.rightArrowKey.isPressed )
+        else if (Keyboard.current.rightArrowKey.isPressed)
         {
             moveInput = 1;
         }
 
         animator.SetBool("isWalking", moveInput != 0);
 
-        if(moveInput > 0)
+        if (moveInput > 0)
         {
             spriteRenderer.flipX = false;
         }
         else if (moveInput < 0)
         {
-            spriteRenderer.flipY = false;
+            spriteRenderer.flipX = true;
         }
     }
 
--- Assets/Scripts/ObjectCatchScript.cs
+++ Assets/Scripts/ObjectCatchScript.cs
@@ -7,28 +7,28 @@ public class ObjectCatchScript : MonoBehaviour
     private Rigidbody2D rb;
     SFX_Script sfx;
 
+
     void Start()
     {
-        sfx = GetComponent<SFX_Script>();
+        sfx = FindFirstObjectByType<SFX_Script>();
         rb = GetComponent<Rigidbody2D>();
     }
+
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.transform.IsChildOf(transform))
             return;
 
-        if (collision.CompareTag("Shout")) {
-            
-                sfx.PlaySFX(4);
-                Destroy(collision.gameObject);
-                transform.localScale += new Vector3(sizeIncrease, sizeIncrease, 0);
-                rb.mass += massIncrease;
+        if (collision.CompareTag("Donut"))
+        {
+            sfx.PlaySFX(4);
+            Destroy(collision.gameObject);
+            transform.localScale += new Vector3(sizeIncrease, sizeIncrease, 0);
+            rb.mass += massIncrease;
 
-            }
-            else
-
+        }
+        else
             Debug.Log("Collided with non-donut object: " + collision.gameObject.name);
     }
-        }
-
+}
